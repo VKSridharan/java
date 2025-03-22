1de871bef5e9664827a2c83ff599f94ab589876a
VKSridharan <sridharanvk123@gmail.com>
2025-03-22 11:43:03 +0530

    Create OddNumberCheck.java

---
+++ OddNumberCheck.java
@@ -0,0 +1,11 @@
+public class OddNumberCheck {
+    public static void main(String[] args) {
+        int number = 7;
+
+        if (number % 2 != 0) {
+            System.out.println(number + " is an odd number.");
+        } else {
+            System.out.println(number + " is not an odd number.");
+        }
+    }
+}
